58d35f523121758d9839c014d7331fd93ef12294
Joshua Briley <josh@thebrileys.com>
2015-12-14 15:22:40 -0500

    HTML layout in place.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function(){
+  // Constants
+  var TAX_RATE = 1.075,
+      PHONE_PRICE = 100.50,
+      ACCESSORY_PRICE = 30.00,
+      BUDGET_BALANCE = 600.00;
+      SPENDING_THRESHOLD = BUDGET_BALANCE * .75;
+
+  function getBudget(){
+
+  }
+
+})();
+
